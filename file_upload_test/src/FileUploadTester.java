329883f1e1912f21e527151e9d4d647c792f23c2
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2014-12-30 18:01:41 +0100

    Removed some non-required stuff

--- file_upload_test/src/FileUploadTester.java
+++ file_upload_test/src/FileUploadTester.java
@@ -1,46 +1,8 @@
-import com.squareup.okhttp.*;
-import com.squareup.okhttp.internal.Util;
-import okio.BufferedSink;
-import okio.Okio;
-import okio.Source;
-
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Paths;
 import java.util.List;
 
 public class FileUploadTester {
-
-    private static RequestBody okHttpTestCreate(final MediaType mediaType, final InputStream inputStream) {
-        return new RequestBody() {
-            @Override
-            public MediaType contentType() {
-                return mediaType;
-            }
-
-            @Override
-            public long contentLength() {
-                try {
-                    return inputStream.available();
-                } catch (IOException e) {
-                    return 0;
-                }
-            }
-
-            @Override
-            public void writeTo(BufferedSink sink) throws IOException {
-                Source source = null;
-                try {
-                    source = Okio.source(inputStream);
-                    sink.writeAll(source);
-                } finally {
-                    Util.closeQuietly(source);
-                }
-            }
-        };
-    }
 
     public static void main(String... args) {
         codeJavaTest();
@@ -72,26 +34,5 @@ public class FileUploadTester {
         } catch (IOException ex) {
             System.err.println(ex);
         }
-    }
-
-    private void okHttpTest() throws IOException {
-        OkHttpClient client = new OkHttpClient();
-
-        MediaType MEDIA_TYPE_MULTIPART
-                = MediaType.parse("application/octet-stream; charset=utf-8");
-
-        System.out.println(Paths.get(".").toAbsolutePath().toString());
-        InputStream inputStream = new FileInputStream("testFile.exe");
-
-        RequestBody requestBody = okHttpTestCreate(MEDIA_TYPE_MULTIPART, inputStream);
-        Request request = new Request.Builder()
-                .url("http://localhost:61000/files?messageid=1&filename=uploadtest.exe")
-                .post(requestBody)
-                .build();
-
-        Response response = client.newCall(request).execute();
-
-        System.out.println("Response status: " + response.code());
-        System.out.println("Response body: " + response.body().string());
     }
 }
